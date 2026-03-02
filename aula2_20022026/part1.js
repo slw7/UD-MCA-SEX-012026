cdc3e5a1ba749db0ae0ba4b578def1a4007eef3c
daniellll06 <daniel2006365@gmail.com>
2026-03-02 00:02:39 -0300

    Add array of peripherals and various console logs

--- aula2_20022026/part1.js
+++ aula2_20022026/part1.js
@@ -1,1 +1,42 @@
-
+let arrayPerifericos= [
+  'teclado',
+  'mouse',
+  'monitor',
+  'processador',
+  'placaMae',
+  'memoriaRAM',
+  'ssd',
+  'hd',
+  'gabinete',
+  'fonte',
+  'headset',
+  'microfone',
+  'webcam',
+  'impressora',
+  'scanner',
+  'roteador',
+  'switch',
+  'notebook',
+  'tablet',
+  'smartphone',
+  'pendrive'
+];
+// A. Elementos nas posições 0, 7, 11, 15, 18 e 20
+console.log(arrayPerifericos[0]);
+console.log(arrayPerifericos[7]);
+console.log(arrayPerifericos[11]);
+console.log(arrayPerifericos[15]);
+console.log(arrayPerifericos[18]);
+console.log(arrayPerifericos[20]);
+// B. Penúltimo e último elemento
+console.log(arrayPerifericos[arrayPerifericos.length - 2]);
+console.log(arrayPerifericos[arrayPerifericos.length - 1]);
+// C. Quantidade de elementos
+console.log("Total de elementos:"[21], arrayPerifericos.length);
+// D. Adicionar novo elemento ao final do array
+arrayPerifericos.push('caixa de som');
+console.log(arrayPerifericos);
+// E. Imprimir todos os elementos usando for
+for (let i = 0; i < arrayPerifericos.length; i++) {
+  console.log(arrayPerifericos[i]);
+}
