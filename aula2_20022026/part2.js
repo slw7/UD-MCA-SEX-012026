b9c65920d914db1dbc166ed4e01a6620d252e555
Roberto <roberto1007077@gmail.com>
2026-03-02 00:08:15 -0300

    Update part2.js

--- aula2_20022026/part2.js
+++ aula2_20022026/part2.js
@@ -22,3 +22,28 @@ for (let chave in fruta) {
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+let produto = {
+  nome: 'Camiseta',
+  cor: 'Azul',
+  preco: 10.00,
+  estoque: 30
+};
+
+// A. Como acessar o nome do objeto
+console.log(produto.nome);
+console.log(produto.[Camiseta]);
+
+// B. Como acessar o preço do objeto utilzando a notação de colchetes
+console.log(produto['preco']);
+console.log(Camiseta['preco']);
+
+
+// C. Atualize o estoque para 80
+produto.estoque = 80;
+console.log("Estoque atualizado:", produto.estoque);
+
+// D. Imprimir todas as propriedades
+for (let chave in produto) {
+  console.log(chave + ": " + produto[chave]);
+}
+
