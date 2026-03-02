838eb520e9429e4dda76ed6292f37d83bb7fcb2e
Roberto <roberto1007077@gmail.com>
2026-03-02 00:08:54 -0300

    Update part2.js

--- aula2_20022026/part2.js
+++ aula2_20022026/part2.js
@@ -47,3 +47,24 @@ for (let chave in produto) {
   console.log(chave + ": " + produto[chave]);
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let Roupas = {
+  nome: 'Camiseta',
+  cor: 'Preta',
+  preco: 59.90,
+  estoque: 120
+};
+// A. Acessar o nome do objeto
+console.log(Roupas.nome);
+// B. Acessar o preço usando colchetes 
+console.log(Roupas['preco']);
+// C. Atualizar o estoque para 80
+Roupas.estoque = 80;
+console.log(Roupas.estoque);
+// D. Imprimir todas as propriedades
+for (let chave in Roupas) {
+  console.log(chave + ": " + Roupas[chave]);
+}
+
+
