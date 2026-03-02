434591f0384ecfaecf87f6a2c4c80717eebb2863
daniellll06 <daniel2006365@gmail.com>
2026-03-02 00:03:00 -0300

    Add Roupas object with properties and methods

--- aula2_20022026/part2.js
+++ aula2_20022026/part2.js
@@ -1,1 +1,19 @@
 
+let Roupas = {
+  nome: 'Camiseta',
+  cor: 'Preta',
+  preco: 59.90,
+  estoque: 120
+};
+// A. Acessar o nome do objeto
+console.log(Roupas.nome);
+// B. Acessar o preço usando colchetes 
+console.log(Roupas['preco']);
+// C. Atualizar o estoque para 80
+Roupas.estoque = 80;
+console.log(Roupas.estoque);
+// D. Imprimir todas as propriedades
+for (let chave in Roupas) {
+  console.log(chave + ": " + Roupas[chave]);
+}
+
